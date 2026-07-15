398f9f7b1861c92f060c978552a8f810510329f7
tuomiorava <tuomiorava@gmail.com>
2026-07-15 13:50:06 +0300

    Updated DoomSquirrel_Re-record bad take - Better handling of take deletion confirmation when not recording. Doesn't start recording, if previous take was not deleted. Improve retake behavior when playing by deleting active take, stopping playback, and starting recording.

--- Recording/DoomSquirrel_Re-record bad take.lua
+++ Recording/DoomSquirrel_Re-record bad take.lua
@@ -18,28 +18,44 @@
 --   Personal Website http://iki.fi/atolonen
 -- @donation
 --   Donate via PayPal https://www.paypal.com/donate/?hosted_button_id=2BEA2GHZMAW9A
--- @version 1.0
+-- @version 1.1
 -- @changelog
---   Initial release
+--   Better handling of take deletion confirmation when not recording. Doesn't start recording, if previous take was not deleted. Improve retake behavior when playing by deleting active take, stopping playback, and starting recording.
 
 local pstate = reaper.GetPlayState()
 
-if (pstate == 5) then
-  -- if recording
+local playing   = (pstate & 1) ~= 0
+local recording = (pstate & 4) ~= 0
 
-  -- Transport: Stop (DELETE all recorded media)
-  reaper.Main_OnCommand(40668, 0)
+if recording then
+    -- Stop and delete recording
+    reaper.Main_OnCommand(40668, 0)
 
-  -- Transport: Record
-  reaper.Main_OnCommand(1013, 0)
+    reaper.defer(function()
+        reaper.Main_OnCommand(1013, 0)
+    end)
+
+elseif playing then
+    -- Delete current take
+    reaper.Main_OnCommand(40129, 0)
+
+    -- Stop playback
+    reaper.Main_OnCommand(1016, 0)
+
+    -- Start recording
+    reaper.defer(function()
+        reaper.Main_OnCommand(1013, 0)
+    end)
+
 else
-  -- if NOT recording
+    local ret = reaper.ShowMessageBox(
+        "Delete the current take and start recording?",
+        "Confirm",
+        4 -- Yes/No
+    )
 
---  -- Take: Delete current take from items
---  reaper.Main_OnCommand(40129, 0)
-  -- Take: Delete current take from items (prompt to confirm)
-  reaper.Main_OnCommand(40130, 0)
-
-  -- Transport: Record
-  reaper.Main_OnCommand(1013, 0)
+    if ret == 6 then
+        reaper.Main_OnCommand(40129, 0)
+        reaper.Main_OnCommand(1013, 0)
+    end
 end
